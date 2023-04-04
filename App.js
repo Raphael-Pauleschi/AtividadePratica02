e013ffd948221851d3122b05872c46a785a78ab8
raphael-pauleschi <raphael.f.pauleschi@gmail.com>
2023-04-04 14:23:25 -0300

    Corrigindo o código

--- App.js
+++ App.js
@@ -1,22 +1,45 @@
-import * as React from 'react';
-import { StyleSheet, View } from 'react-native';
-import { StatusBar } from 'expo-status-bar';
-import Valores from './src/components/Valores';
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button, FlatList } from 'react-native';
+import styles from './src/components/Style';
 
 export default function App() {
+  const [min, setMin] = useState(null);
+  const [max, setMax] = useState(null);
+  const [numeros, setNumeros] = useState([]);
+
+  function gerarNumerosAleatorios() {
+    if (min && max) {
+      const novoArray = [];
+      for (let i = 0; i < 5; i++) {
+        const numero = Math.floor(Math.random() * (max - min + 1)) + min;
+        novoArray.push(numero);
+      }
+      setNumeros(novoArray);
+    }
+  }
+
   return (
     <View style={styles.container}>
-      <Valores/>
-      <Valores/>
-      <StatusBar style="auto" />
+      <Text style={styles.label}>Valor mínimo:</Text>
+      <TextInput
+        style={styles.input}
+        value={min}
+        onChangeText={valor => setMin(parseInt(valor))}
+        keyboardType="numeric"
+      />
+      <Text style={styles.label}>Valor máximo:</Text>
+      <TextInput
+        style={styles.input}
+        value={max}
+        onChangeText={valor => setMax(parseInt(valor))}
+        keyboardType="numeric"
+      />
+      <Button title="Gerar números aleatórios" onPress={gerarNumerosAleatorios} />
+      <FlatList
+        data={numeros}
+        keyExtractor={item => item.toString()}
+        renderItem={({ item }) => <Text style={styles.item}>{item}</Text>}
+      />
     </View>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'green',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/components/Style.js
@@ -0,0 +1,30 @@
+import {StyleSheet} from 'react-native';
+
+export default StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+    backgroundColor: '#f77ce3',
+  },
+  label: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginTop: 20,
+  },
+  input: {
+    height: 40,
+    width: '80%',
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginTop: 10,
+    marginBottom: 20,
+    paddingLeft: 10,
+  },
+  item: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginTop: 10,
+  },
+});
